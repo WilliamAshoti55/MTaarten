c451f81df2d4ec77e7a581cb44436d14e8eccea4
TestautomationWA <47525977+TestautomationWA@users.noreply.github.com>
2020-04-23 23:15:34 +0200

    saving the bruidstaart data into database also create a file for the bruidstaarten

--- routes/bruidstaart.js
+++ routes/bruidstaart.js
@@ -4,25 +4,45 @@ module.exports = {
     addBruidstaartPage: (req, res) => {
         res.render('addBruidstaart.ejs', {
             title: 'Welkom op de pagina Bruidstaarten',
-            message: 'Test'
+            message: 'Vroeeemmmmmmmmmmmmmmm'
         });
     },
     addBruidstaart: (req,res) => {
 
-        // if (!req.files) {
-        //     return res.status(400).send("No files were uploaded.");
-        // }
+        if (!req.files) {
+            return res.status(400).send("No files were uploaded.");
+        }
 
-        // let message = '';
-        // let bruidstaartnaam = req.body.bruidstaartnaam;
-        // let aantalpersonen = req.body.aantalpersonen;
-        // let uploadedFile    = req.files.image;
-        // let image_name      = uploadedFile.name;
-        // let fileExtension   = uploadedFile.mimetype.split('/')[1];
-        // image_name          = bruidstaartnaam + '.' + fileExtension;
-        res.render('addBruidstaart.ejs' , {
-            title: 'Welkom op de pagina Bruidstaarten',
-            message: 'Test'
-        });
-    }
-}
+        let message = '';
+        let bruidstaartnaam = req.body.bruidstaartnaam;
+        let aantalpersonen = req.body.aantalpersonen;
+        let uploadedFile    = req.files.image;
+        let image_name      = uploadedFile.name;
+        let fileExtension   = uploadedFile.mimetype.split('/')[1];
+        image_name          = bruidstaartnaam + '.' + fileExtension;
+
+
+        if(uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
+            // upload the file to the /public/assets/img directory
+            uploadedFile.mv(`public/assets/bruidstaartenmap/${image_name}`, (err ) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+                // send the player's details to the database
+                let query = "INSERT INTO `bruidstaarten` (bruidstaartnaam, aantalpersonen, image) VALUES ('" + bruidstaartnaam + "', '" + aantalpersonen + "', '" + image_name + "')";
+                db.query(query, (err, result) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+                    res.redirect('/homepageMTaartenAdmin');
+                });
+            });
+        } else {
+            message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
+            res.render('addBruidstaart.ejs', {
+                message,
+                title: 'Welcome to Socka | Add a new player'
+            });
+        }
+
+}}
