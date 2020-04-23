bb4ecc937ad7bbe8f2228b8036e0297247d94379
TestautomationWA <47525977+TestautomationWA@users.noreply.github.com>
2020-04-23 21:42:53 +0200

    added some changes

--- routes/bruidstaart.js
+++ routes/bruidstaart.js
@@ -9,17 +9,17 @@ module.exports = {
     },
     addBruidstaart: (req,res) => {
 
-        if (!req.files) {
-            return res.status(400).send("No files were uploaded.");
-        }
+        // if (!req.files) {
+        //     return res.status(400).send("No files were uploaded.");
+        // }
 
-        let message = '';
-        let bruidstaartnaam = req.body.bruidstaartnaam;
-        let aantalpersonen = req.body.aantalpersonen;
-        let uploadedFile    = req.files.image;
-        let image_name      = uploadedFile.name;
-        let fileExtension   = uploadedFile.mimetype.split('/')[1];
-        image_name          = bruidstaartnaam + '.' + fileExtension;
+        // let message = '';
+        // let bruidstaartnaam = req.body.bruidstaartnaam;
+        // let aantalpersonen = req.body.aantalpersonen;
+        // let uploadedFile    = req.files.image;
+        // let image_name      = uploadedFile.name;
+        // let fileExtension   = uploadedFile.mimetype.split('/')[1];
+        // image_name          = bruidstaartnaam + '.' + fileExtension;
         res.render('addBruidstaart.ejs' , {
             title: 'Welkom op de pagina Bruidstaarten',
             message: 'Test'
